7b3233d44d8dbd18534cf2de6970b5dfe0741524
Chance Stephens <chance.stephens2022@gmail.com>
2024-08-06 08:25:04 -0700

    Updated Styling

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import memeProjectImage from '../images/capstonelvl3Sample.png'
-import memeCollectionImage from '../images/capstonelvl3Collection.png'
-
+import imgPlaceholder from '../images/imgPlaceholder.jpg'
 export default function Projects() {
 
     return(
-        <div className='myProjects'>
-            {/* <h1>Meme Generator</h1>
-            <div className='myProjectInnerContainer'>
-                <p>"Memer's Paradise: A React-powered meme generator where users can easily add custom captions to pre-loaded images, fostering creativity and humor in meme creation."</p>
-            </div>
-            <div className='imageContainer'>
-                <img src={memeProjectImage} />
-                <p>Have a random meme image generate and use your wit to create a meme.</p>
-            </div>
-            <div className='imageContainer'>
-                <img src={memeCollectionImage} />
-                <p>As a user, you can save your memes to a collection where you can edit or delete them.</p>
-            </div> */}
+        <div className='projects'>
+            <p>Goal Getter</p>
+            <img src={imgPlaceholder} />
+            <p>NFL Roadmap</p>
+            <img src={imgPlaceholder} />
+            <p>Memer's Paradise</p>
+            <img src={imgPlaceholder} />
 
         </div>
     )
